a3576863c0c1d6c6591fab92d24046b7eef71456
zrj570543941 <570543941@qq.com>
2018-01-15 08:36:30 +0800

    reddit demo change

--- angular-reddit/src/app/app.component.ts
+++ angular-reddit/src/app/app.component.ts
@@ -6,5 +6,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  addArticle(e) {
+    e.preventDefault();S
+    console.log(111);
+  }
 }
