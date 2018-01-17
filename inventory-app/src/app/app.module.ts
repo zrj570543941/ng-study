6ee36b8e097ef14b9f6d134ff3f1fd90a3693e81
zrj570543941 <570543941@qq.com>
2018-01-17 19:49:47 +0800

    init inventory-app

---
+++ inventory-app/src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+
+import { AppComponent } from './app.component';
+import { ProductsListComponent } from './products-list/products-list.component';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ProductsListComponent,
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
