a1a6f42e5b92caec0c8782886164b6f8d6d16e64
zrj570543941 <570543941@qq.com>
2018-01-19 08:38:18 +0800

    init form-demo1

---
+++ form-demo1/src/app/app.module.ts
@@ -0,0 +1,23 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
+
+
+import { AppComponent } from './app.component';
+import { DemoFormSkuComponent } from './demo-form-sku/demo-form-sku.component';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    DemoFormSkuComponent,
+  ],
+  imports: [
+    BrowserModule,
+    ReactiveFormsModule,
+    FormsModule,
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
